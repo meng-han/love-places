cfec0ef30e8ba90d2a19b0a8824260c8404f7cba
menghan2 <menghan2@illinois.edu>
2016-03-02 10:45:45 -0600

    First commit. Added Google Maps API and Google Places API.

--- Love Places/AppDelegate.m
+++ Love Places/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+@import GoogleMaps;
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [GMSServices provideAPIKey:@"AIzaSyDUl6r4yXSIPHfny0rRmDhW0JlaD3IoEMs"];
     return YES;
 }
 
--- Love Places/ViewController.m
+++ Love Places/ViewController.m
@@ -7,21 +7,75 @@
 //
 
 #import "ViewController.h"
+@import GoogleMaps;
 
 @interface ViewController ()
-
+// Instantiate a pair of UILabels in Interface Builder
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
 @end
 
 @implementation ViewController
+{
+    GMSPlacesClient *_placesClient;
+    GMSPlacePicker *_placePicker;
 
+
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _placesClient = [[GMSPlacesClient alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)pickPlace:(UIButton *)sender {
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(51.5108396, -0.0922251);
+    CLLocationCoordinate2D northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001);
+    CLLocationCoordinate2D southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001);
+    GMSCoordinateBounds *viewport = [[GMSCoordinateBounds alloc] initWithCoordinate:northEast
+                                                                         coordinate:southWest];
+    GMSPlacePickerConfig *config = [[GMSPlacePickerConfig alloc] initWithViewport:viewport];
+    _placePicker = [[GMSPlacePicker alloc] initWithConfig:config];
+    
+    [_placePicker pickPlaceWithCallback:^(GMSPlace *place, NSError *error) {
+        if (error != nil) {
+            NSLog(@"Pick Place error %@", [error localizedDescription]);
+            return;
+        }
+        
+        if (place != nil) {
+            NSLog(@"Place name %@", place.name);
+            NSLog(@"Place address %@", place.formattedAddress);
+            NSLog(@"Place attributions %@", place.attributions.string);
+        } else {
+            NSLog(@"No place selected");
+        }
+    }];
+}
+
+- (IBAction)getCurrentPlace:(UIButton *)sender {
+    [_placesClient currentPlaceWithCallback:^(GMSPlaceLikelihoodList *placeLikelihoodList, NSError *error){
+        if (error != nil) {
+            NSLog(@"Pick Place error %@", [error localizedDescription]);
+            return;
+        }
+        
+        self.nameLabel.text = @"No current place";
+        self.addressLabel.text = @"";
+        
+        if (placeLikelihoodList != nil) {
+            GMSPlace *place = [[[placeLikelihoodList likelihoods] firstObject] place];
+            if (place != nil) {
+                self.nameLabel.text = place.name;
+                self.addressLabel.text = [[place.formattedAddress componentsSeparatedByString:@", "]
+                                          componentsJoinedByString:@"\n"];
+            }
+        }
+    }];
 }
 
 @end
