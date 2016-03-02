6f74e05133108d4d6a24903a2f2f1765e9fb652b
menghan2 <menghan2@illinois.edu>
2016-03-02 09:57:53 -0600

    Initial Commit

---
+++ Love Places/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Love Places
+//
+//  Created by Meng Han on 3/2/16.
+//  Copyright © 2016 menghan2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Love PlacesTests/Love_PlacesTests.m
@@ -0,0 +1,39 @@
+//
+//  Love_PlacesTests.m
+//  Love PlacesTests
+//
+//  Created by Meng Han on 3/2/16.
+//  Copyright © 2016 menghan2. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Love_PlacesTests : XCTestCase
+
+@end
+
+@implementation Love_PlacesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
